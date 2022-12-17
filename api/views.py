ee00e64e95b67c6c5927d61090760604f720cf91
saqib <saqib29111@gmail.com>
2022-12-18 01:49:56 +0600

    views

--- api/views.py
+++ api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Item, Location
+from .serializers import ItemSerializer, LocationSerializer
+
+class ItemList(generics.ListCreateAPIView):
+    serializer_class = ItemSerializer
+
+    def get_queryset(self):
+        queryset = Item.objects.all()
+        Location = self.request.query_params.get('location')
+        if Location is not None:
+            queryset = queryset.filter(itemLocation=Location)
+        
+        return queryset
+
+class ItemDetails(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ItemSerializer
+    queryset = Item.objects.all()
+
+class LocationList(generics.ListCreateAPIView):
+    serializer_class = LocationSerializer
+    queryset = Location.objects.all()
+
+class LocationDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = LocationSerializer
+    queryset = Location.objects.all()
